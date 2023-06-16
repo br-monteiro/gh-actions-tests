b173ddcd6c6cbb3b0ac6805e940c66dfdc79c365
Edinho Monteiro <bruno.monteirodg@gmail.com>
2023-06-16 17:09:38 -0300

    teste

---
+++ .deploy/bin/trigger.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ENVIRONMENT=${1:-$ENVIRONMENT}
+TAG=${2:-$TAG}
+THEME=${6:-$THEME}
+NAMESPACE=${3:-$NAMESPACE}
+
+config_file=k8s-$ENVIRONMENT-$TAG.txt
+
+envsubst < .deploy/$THEME/$ENVIRONMENT-resources.txt > $config_file
+
+cat $config_file
